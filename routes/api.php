4e015cc9f4356278e701627ff6bc62d37c5db7f9
Rodrigo Silva <rodsdev@gmail.com>
2024-10-13 20:49:30 -0300

    Adiciona rota para /video

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Video\VideoController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
@@ -11,4 +12,8 @@ Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/videos', [VideoController::class, 'store']);
 });
